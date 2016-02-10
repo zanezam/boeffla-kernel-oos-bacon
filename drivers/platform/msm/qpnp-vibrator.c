87f2383b326081e44e734a559da9577dadc687d5
andip71 <andreasp@gmx.de>
2016-02-10 18:37:00 +0100

    qpnp-vibrator: Add sysfs interface to control intensity by levels (0...x) instead of offset levels
        Sysfs:
    
            /sys/devices/virtual/timed_output/vibrator/level
    
            values:
                    0...20 (20 is default, 0 is off, 20 is max)

--- drivers/platform/msm/qpnp-vibrator.c
+++ drivers/platform/msm/qpnp-vibrator.c
@@ -31,6 +31,10 @@
 #define QPNP_VIB_DEFAULT_VTG_MAX	3100
 #define QPNP_VIB_DEFAULT_VTG_MIN	1200
 
+#define BOEFFLA_LEVEL_MIN			0
+#define BOEFFLA_LEVEL_MAX			20
+#define BOEFFLA_LEVEL_SHIFT			11
+
 #define QPNP_VIB_EN			BIT(7)
 #define QPNP_VIB_VTG_SET_MASK		0x1F
 #define QPNP_VIB_LOGIC_SHIFT		4
@@ -48,6 +52,7 @@ struct qpnp_vib {
 	int vtg_min;
 	int vtg_max;
 	int vtg_level;
+	int level;
 	int timeout;
 	spinlock_t lock;
 };
@@ -117,6 +122,48 @@ static ssize_t qpnp_vib_level_store(struct device *dev,
         return strnlen(buf, count);
 }
 
+static ssize_t qpnp_boeffla_vib_level_show(struct device *dev,
+                                        struct device_attribute *attr,
+                                        char *buf)
+{
+        struct timed_output_dev *tdev = dev_get_drvdata(dev);
+        struct qpnp_vib *vib = container_of(tdev, struct qpnp_vib,
+                                         timed_dev);
+
+        return scnprintf(buf, PAGE_SIZE, "%d\n", vib->vtg_level - BOEFFLA_LEVEL_SHIFT);
+}
+
+
+static ssize_t qpnp_boeffla_vib_level_store(struct device *dev,
+                                        struct device_attribute *attr,
+                                        const char *buf, size_t count)
+{
+        struct timed_output_dev *tdev = dev_get_drvdata(dev);
+        struct qpnp_vib *vib = container_of(tdev, struct qpnp_vib,
+                                         timed_dev);
+        int val;
+        int rc;
+
+        rc = kstrtoint(buf, 10, &val);
+        if (rc) {
+                pr_err("%s: error getting level\n", __func__);
+                return -EINVAL;
+        }
+
+        if (val < BOEFFLA_LEVEL_MIN) {
+                pr_err("%s: level %d not in range (%d - %d), using min.", __func__, val, vib->vtg_min, vib->vtg_max);
+                val = BOEFFLA_LEVEL_MIN;
+        } else if (val > BOEFFLA_LEVEL_MAX) {
+                pr_err("%s: level %d not in range (%d - %d), using max.", __func__, val, vib->vtg_min, vib->vtg_max);
+                val = BOEFFLA_LEVEL_MAX;
+        }
+
+        vib->vtg_level = val + BOEFFLA_LEVEL_SHIFT;
+
+        return strnlen(buf, count);
+}
+
+static DEVICE_ATTR(level, S_IRUGO | S_IWUSR, qpnp_boeffla_vib_level_show, qpnp_boeffla_vib_level_store);
 static DEVICE_ATTR(vtg_level, S_IRUGO | S_IWUSR, qpnp_vib_level_show, qpnp_vib_level_store);
 static DEVICE_ATTR(vtg_min, S_IRUGO, qpnp_vib_min_show, NULL);
 static DEVICE_ATTR(vtg_max, S_IRUGO, qpnp_vib_max_show, NULL);
@@ -199,7 +246,7 @@ static int qpnp_vib_set(struct qpnp_vib *vib, int on)
 	int rc;
 	u8 val;
 
-	if (on) {
+	if ((on) && ((vib->vtg_level - BOEFFLA_LEVEL_SHIFT) > 0)) {
 		val = vib->reg_vtg_ctl;
 		val &= ~QPNP_VIB_VTG_SET_MASK;
 		val |= (vib->vtg_level & QPNP_VIB_VTG_SET_MASK);
@@ -397,6 +444,7 @@ static int __devinit qpnp_vibrator_probe(struct spmi_device *spmi)
 	if (rc < 0)
 		return rc;
 
+		device_create_file(vib->timed_dev.dev, &dev_attr_level);
 		device_create_file(vib->timed_dev.dev, &dev_attr_vtg_level);
 		device_create_file(vib->timed_dev.dev, &dev_attr_vtg_min);
 		device_create_file(vib->timed_dev.dev, &dev_attr_vtg_max);
